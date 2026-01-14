24c4ad8286b7e8d01246bf2377c09abeac2eff20
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-01-14 20:13:36 +0100

    Sistemati file index e js finali

--- script/script.js
+++ script/script.js
@@ -1,21 +1,28 @@
 let numeri=[];
 function salva(){
-    let n=parseFloat(document.getElementById('numero'));
+    let n=parseFloat(document.getElementById('numero').value);
     numeri.push(n);
+    document.getElementById('inseriti').innerHTML=numeri.join(";");
 }
 function esegui(){
     let somma=0;
     let prodotto=1;
+    if(numeri.length==0){ 
+        alert('Inserisci almeno un numero'); 
+        return;
+    }
     for(let pari of numeri){
         if(pari%2==0){
             somma+=pari;
         }
-        for(let dispari in numeri){
-        if(dispari%2!=0){
-            prodotto=prodotto*pari;
+    }
+        for(let i in numeri){
+        if(i % 2==0){        //Ho fatto in posizione dispari vista dall'utente, Se fosse stata pos dispari dell'array allora ==1
+            prodotto*=numeri[i];
         }
     }
-    }
-    alert(somma);
-    alert(prodotto);
+    numeri.length=0;
+    document.getElementById('ris').innerHTML="Somma pari: " + somma + "<br>" +
+                                             "Prodotto dispari: " +prodotto;
+
 }
